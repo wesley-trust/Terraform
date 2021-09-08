e7fff7c4b9d2880fea769096ae4a2752133d703d
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-08 16:45:56 +0100

    Update variable

--- Deployments/Resource_group/examples/variables.tf
+++ Deployments/Resource_group/examples/variables.tf
@@ -14,7 +14,6 @@ variable "service_environment" {
 variable "service_deployment" {
   description = "Desired deployment identifier of the service collection of provisioned resources"
   type        = string
-  default = "01"
 }
 
 variable "service_location" {
--- Deployments/Resource_group/tests/integration_test.go
+++ Deployments/Resource_group/tests/integration_test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"testing"
-	
+
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -14,13 +14,16 @@ func TestIntegrationExample(t *testing.T) {
 	// Generate a random ID to prevent a naming conflict
 	uniqueID := random.UniqueId()
 
-	// Concatenate resource group name
+	// Concatenate expected resource group name
 	resourceGroupName := "Services-Test-UKS-" + uniqueID + "-rg"
 
 	// Enable retryable error
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
 		// The path to where the Terraform code is located
 		TerraformDir: "../examples/",
+
+		// Variables to pass to the Terraform code using -var options
 		Vars: map[string]interface{}{
 			"service_deployment": uniqueID,
 		},
