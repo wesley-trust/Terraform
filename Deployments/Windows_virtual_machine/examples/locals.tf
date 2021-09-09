d26f0d3c07afc664a70cd096d137a25c2560aa44
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-09 18:51:47 +0100

    Update locals

--- Deployments/Windows_virtual_machine/examples/locals.tf
+++ Deployments/Windows_virtual_machine/examples/locals.tf
@@ -7,6 +7,4 @@ locals {
   resource_locations                        = lookup(var.service_location, "TEST", null)
   resource_instance_size                    = lookup(lookup(var.resource_instance_size, "TEST", null), var.service_name, null)
   resource_instance_count                   = lookup(lookup(var.resource_instance_count, "TEST", null), var.service_name, null)
-  resource_recovery_services_locations      = lookup(var.service_recovery_services_location, "TEST", null)
-  resource_recovery_services_instance_count = lookup(lookup(var.resource_recovery_services_instance_count, "TEST", null), var.service_name, null)
 }
