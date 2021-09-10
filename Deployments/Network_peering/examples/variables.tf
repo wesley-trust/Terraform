93478af4ecabac8a8f01213645372667cfb7a99c
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-10 09:57:03 +0100

    Update test

--- Deployments/Network_peering/examples/variables.tf
+++ Deployments/Network_peering/examples/variables.tf
@@ -11,12 +11,6 @@ variable "service_environment" {
   default     = "Test"
 }
 
-variable "service_deployment" {
-  description = "Desired deployment identifier of the service collection of provisioned resources"
-  type        = string
-  default     = "01"
-}
-
 variable "service_location" {
   description = "The production resource locations to deploy"
   type        = map(any)
@@ -28,8 +22,60 @@ variable "service_location" {
   }
 }
 
-variable "resource_network_peer_role" {
-  description = "The peering type"
+variable "service_deployment" {
+  description = "Desired deployment identifier of the service collection of provisioned resources"
   type        = string
-  default     = "spoke"
+}
+
+variable "resource_name" {
+  description = "Desired name for the provisioned resources"
+  type        = map(string)
+  default = {
+    "Services" = "TST"
+  }
+}
+
+variable "resource_instance_count" {
+  description = "Desired number of the provisioned resources for each service environment"
+  type        = map(any)
+  default = {
+    Test = {
+      "Services" = 2
+    }
+  }
+}
+
+variable "resource_instance_size" {
+  description = "Desired size for the provisioned resources for each service"
+  type        = map(any)
+  default = {
+    Test = {
+      "Services" = "Standard_B1s"
+    }
+  }
+}
+
+variable "resource_address_space" {
+  description = "Desired address space for the provisioned resources"
+  type        = map(string)
+  default = {
+    "UK South"         = "10.0.2.0/24"
+    "North Central US" = "10.6.2.0/24"
+  }
+}
+
+variable "resource_dns_servers" {
+  description = "Desired DNS servers for the provisioned resources"
+  type        = map(any)
+  default = {
+    "UK South" = [
+      "10.0.2.4",
+      "10.0.2.5"
+    ]
+
+    "North Central US" = [
+      "10.6.2.4",
+      "10.6.2.5"
+    ]
+  }
 }
