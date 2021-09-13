35eff74fa5e6e48a3b99bc7b6e35461f342d79fb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 09:38:24 +0100

    Update tests [skip ci]

--- Deployments/Windows_virtual_machine/tests/integration_test.go
+++ Deployments/Windows_virtual_machine/tests/integration_test.go
@@ -99,6 +99,37 @@ func TestSingleInstanceSingleRegionWithMultiNI(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 }
 
+func TestMultiInstanceSingleRegionWithLB(t *testing.T) {
+	t.Parallel()
+
+	// Generate a random ID to prevent a naming conflict
+	uniqueID := random.UniqueId()
+
+	// Define variables
+	locations := []string{"UK South"}
+
+	// Enable retryable error
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// The path to where the Terraform code is located
+		TerraformDir: "../examples/",
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"service_deployment": uniqueID,
+			"resource_instance_count": 2,
+			"service_location": locations,
+			"provision_public_load_balancer": true,
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+}
+
 func TestMultiInstanceMultiRegion(t *testing.T) {
 	t.Parallel()
 
@@ -119,37 +150,6 @@ func TestMultiInstanceMultiRegion(t *testing.T) {
 			"service_deployment": uniqueID,
 			"resource_instance_count": 2,
 			"service_location": locations,
-		},
-	})
-
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
-
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
-	terraform.InitAndApply(t, terraformOptions)
-}
-
-func TestMultiInstanceMultiRegionWithLB(t *testing.T) {
-	t.Parallel()
-
-	// Generate a random ID to prevent a naming conflict
-	uniqueID := random.UniqueId()
-
-	// Define variables
-	locations := []string{"UK South", "North Central US"}
-
-	// Enable retryable error
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-
-		// The path to where the Terraform code is located
-		TerraformDir: "../examples/",
-
-		// Variables to pass to the Terraform code using -var options
-		Vars: map[string]interface{}{
-			"service_deployment": uniqueID,
-			"resource_instance_count": 2,
-			"service_location": locations,
-			"provision_public_load_balancer": true,
 		},
 	})
 
