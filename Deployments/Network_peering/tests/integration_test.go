de4f47b09e89710a484885b3f677a0607d54762d
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 14:12:16 +0100

    Update tests [skip ci]

--- Deployments/Network_peering/tests/integration_test.go
+++ Deployments/Network_peering/tests/integration_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	//"github.com/stretchr/testify/assert"
 )
 
 func TestIntegrationExample(t *testing.T) {
@@ -14,11 +13,11 @@ func TestIntegrationExample(t *testing.T) {
 	// Generate a random ID to prevent a naming conflict
 	uniqueID := random.UniqueId()
 
-	// Concatenate expected resource group name
-	//resourceGroupName := "Services-Test-UKS-" + uniqueID + "-rg"
-
 	// Enable retryable error
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// Set Parallelism to 1
+		Parallelism: 1,
 
 		// The path to where the Terraform code is located
 		TerraformDir: "../examples/",
@@ -27,11 +26,6 @@ func TestIntegrationExample(t *testing.T) {
 		Vars: map[string]interface{}{
 			"service_deployment": uniqueID,
 		},
-
-		// Set Parallelism to 1
-		Parallelism: int{
-			"1",
-		},
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
@@ -39,8 +33,4 @@ func TestIntegrationExample(t *testing.T) {
 
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
-
-	// Run `terraform output` to get the values of output variables
-	//output := terraform.Output(t, terraformOptions, "resourceGroupName")
-	//assert.Equal(t, resourceGroupName, output)
 }
