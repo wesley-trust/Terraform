c48f866b6579bb07089e4ece41602160f8465fcf
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 19:19:35 +0100

    Update [skip ci]

--- Deployments/Network_peering/examples/resources.tf
+++ Deployments/Network_peering/examples/resources.tf
@@ -1,21 +1,21 @@
 module "network_peering_spoke" {
-  for_each                   = toset(local.resource_locations)
+  for_each                   = toset(var.service_location)
   source                     = "../"
   service_environment        = var.service_environment
   service_deployment         = "${var.service_deployment}-Spoke"
   service_name               = var.service_name
   service_location           = each.value
-  resource_network_peer      = var.virtual_machine_spoke[each.value]
+  resource_network_peer      = var.service_network_spoke[each.value]
   resource_network_peer_role = "hub"
 }
 
 module "network_peering_hub" {
-  for_each                   = toset(local.resource_locations)
+  for_each                   = toset(var.service_location)
   source                     = "../"
   service_environment        = var.service_environment
   service_deployment         = "${var.service_deployment}-Hub"
   service_name               = var.service_name
   service_location           = each.value
-  resource_network_peer      = var.virtual_machine_hub[each.value]
+  resource_network_peer      = var.service_network_hub[each.value]
   resource_network_peer_role = "spoke"
 }
