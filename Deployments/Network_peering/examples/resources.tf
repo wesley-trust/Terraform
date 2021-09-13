5759fc54a730a66423ab32b66d41a8513dc6af4f
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 16:56:58 +0100

    Update tests [skip ci]

--- Deployments/Network_peering/examples/resources.tf
+++ Deployments/Network_peering/examples/resources.tf
@@ -1,33 +1,3 @@
-module "windows_virtual_machine_spoke" {
-  for_each                = toset(local.resource_locations)
-  source                  = "../../Windows_virtual_machine"
-  service_environment     = var.service_environment
-  service_deployment      = "${var.service_deployment}-Spoke"
-  service_name            = var.service_name
-  service_location        = each.value
-  resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
-  resource_instance_size  = local.resource_instance_size
-  resource_address_space  = lookup(var.resource_address_space, each.value, null)
-  resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
-  resource_network_role   = "spoke"
-}
-
-module "linux_virtual_machine_hub" {
-  for_each                = toset(local.resource_locations)
-  source                  = "../../Linux_virtual_machine"
-  service_environment     = var.service_environment
-  service_deployment      = "${var.service_deployment}-Hub"
-  service_name            = var.service_name
-  service_location        = each.value
-  resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
-  resource_instance_size  = local.resource_instance_size
-  resource_address_space  = lookup(var.resource_address_space, each.value, null)
-  resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
-  resource_network_role   = "hub"
-}
-
 module "network_peering_spoke" {
   for_each                   = toset(local.resource_locations)
   source                     = "../"
--- Deployments/Network_peering/tests/integration_test.go
+++ Deployments/Network_peering/tests/integration_test.go
@@ -11,13 +11,30 @@ func TestIntegrationExample(t *testing.T) {
 	t.Parallel()
 
 	// Generate a random ID to prevent a naming conflict
-	uniqueID := random.UniqueId()
+	uniqueID := random.UniqueId()#
 
+	// Deploy dependencies
+	// Enable retryable error
+	terraformDependencyOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// The path to where the Terraform code is located
+		TerraformDir: "../examples/dependencies",
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"service_deployment": uniqueID,
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformDependencyOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformDependencyOptions)
+
+	// Deploy module
 	// Enable retryable error
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-
-		// Set Parallelism to 1
-		Parallelism: 1,
 
 		// The path to where the Terraform code is located
 		TerraformDir: "../examples/",
@@ -29,7 +46,7 @@ func TestIntegrationExample(t *testing.T) {
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	//defer terraform.Destroy(t, terraformOptions)
 
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
