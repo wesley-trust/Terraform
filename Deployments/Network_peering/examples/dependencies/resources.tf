f261f60f00aab5a8f2ab0b2b5fe829b6e00edd76
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 17:24:42 +0100

    Update [skip ci]

--- Deployments/Network_peering/examples/dependencies/resources.tf
+++ Deployments/Network_peering/examples/dependencies/resources.tf
@@ -1,12 +1,12 @@
 module "virtual_machine_spoke" {
-  for_each                = toset(local.resource_locations)
+  for_each                = toset(var.service_location)
   source                  = "../../../Windows_virtual_machine"
   service_environment     = var.service_environment
   service_deployment      = "${var.service_deployment}-Spoke"
   service_name            = var.service_name
   service_location        = each.value
   resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
+  resource_instance_count = var.resource_instance_count
   resource_instance_size  = local.resource_instance_size
   resource_address_space  = lookup(var.resource_address_space, each.value, null)
   resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
@@ -14,14 +14,14 @@ module "virtual_machine_spoke" {
 }
 
 module "virtual_machine_hub" {
-  for_each                = toset(local.resource_locations)
+  for_each                = toset(var.service_location)
   source                  = "../../../Linux_virtual_machine"
   service_environment     = var.service_environment
   service_deployment      = "${var.service_deployment}-Hub"
   service_name            = var.service_name
   service_location        = each.value
   resource_name           = local.resource_name
-  resource_instance_count = local.resource_instance_count
+  resource_instance_count = var.resource_instance_count
   resource_instance_size  = local.resource_instance_size
   resource_address_space  = lookup(var.resource_address_space, each.value, null)
   resource_dns_servers    = lookup(var.resource_dns_servers, each.value, null)
