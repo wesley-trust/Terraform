1be5ff568d91c5aa825d665bea7e4ad9f86ec0bb
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-13 18:33:08 +0100

    Update [skip ci]

--- Deployments/Network_peering/tests/unit_test.go
+++ Deployments/Network_peering/tests/unit_test.go
@@ -35,8 +35,8 @@ func TestLocalNetworkPeering(t *testing.T) {
 	terraform.InitAndPlan(t, terraformDependencyOptions)
 
 	// Define outputs
-	virtualMachineSpoke := terraform.Output(t, terraformDependencyOptions, "virtual_machine_spoke")
-	virtualMachineHub := terraform.Output(t, terraformDependencyOptions, "virtual_machine_hub")
+	virtualMachineSpoke := terraform.OutputMap(t, terraformDependencyOptions, "virtual_machine_spoke")
+	virtualMachineHub := terraform.OutputMap(t, terraformDependencyOptions, "virtual_machine_hub")
 
 	// Plan module
 	// Enable retryable error
