39d5e1aba6267c7ae9fdf413b7cb0af114724bb5
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-14 17:25:33 +0100

    Update [skip ci]

--- Deployments/Network_peering/examples/resources.tf
+++ Deployments/Network_peering/examples/resources.tf
@@ -5,7 +5,7 @@ module "network_peering_spoke" {
   service_deployment         = "${var.service_deployment}-Spoke"
   service_name               = var.service_name
   service_location           = each.value
-  resource_network_peer      = var.service_network_spoke[each.key]
+  resource_network_peer      = var.service_network_spoke[index(toset(var.service_location), each.key)]
   #resource_network_peer      = var.service_network_spoke[index(toset(var.service_location), each.value)]
   #resource_network_peer      = var.service_network_spoke[index(keys(toset(var.service_location)), each.key)]
   #resource_network_peer      = element(var.service_network_spoke, index(toset(var.service_location), each.key))
@@ -19,7 +19,8 @@ module "network_peering_hub" {
   service_deployment         = "${var.service_deployment}-Hub"
   service_name               = var.service_name
   service_location           = each.value
-  resource_network_peer      = var.service_network_hub[each.key]
+  resource_network_peer      = var.service_network_hub[index(toset(var.service_location), each.key)]
+  #resource_network_peer      = var.service_network_hub[each.key]
   #resource_network_peer      = var.service_network_hub[index(keys(var.service_location), each.key)]
   #resource_network_peer      = var.service_network_hub[index(toset(var.service_location), each.value)]
   #resource_network_peer      = var.service_network_hub[index(keys(toset(var.service_location)), each.key)]
