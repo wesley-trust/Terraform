9f746acf2672f75a9e9ec09cfe0191be45fe3275
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-16 18:26:05 +0100

    Update case

--- Resources/Key_vault/resources_key_vault.tf
+++ Resources/Key_vault/resources_key_vault.tf
@@ -14,15 +14,15 @@ resource "azurerm_key_vault" "vault" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "get",
+      "Get",
     ]
 
     secret_permissions = [
-      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
     ]
 
     storage_permissions = [
-      "get",
+      "Get",
     ]
   }
 
@@ -34,15 +34,15 @@ resource "azurerm_key_vault" "vault" {
       object_id = access_policy.value
 
       key_permissions = [
-        "get",
+        "Get",
       ]
 
       secret_permissions = [
-        "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+        "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
       ]
 
       storage_permissions = [
-        "get",
+        "Get",
       ]
     }
   }
