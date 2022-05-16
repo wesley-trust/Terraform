33d3fa3a7c6b8fb2efbf88b38d43fa71b24dd826
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-16 20:55:52 +0100

    Add comment

--- Deployments/Windows_virtual_machine/examples/providers.tf
+++ Deployments/Windows_virtual_machine/examples/providers.tf
@@ -15,6 +15,7 @@ provider "azurerm" {
       skip_shutdown_and_force_delete = true
     }
     
+    # Workaround OS disk being retained after VM delete operation
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
