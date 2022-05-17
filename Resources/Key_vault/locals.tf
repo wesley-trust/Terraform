9448c6e558326b61a02d10fc8fd2c212ce75224d
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-05-17 09:58:32 +0100

    Add deployment ID to name

--- Resources/Key_vault/locals.tf
+++ Resources/Key_vault/locals.tf
@@ -2,5 +2,5 @@
 locals {
 
   # Resource functions and concatenations
-  resource_key_vault_name = lower("${var.resource_name}-${random_id.id.hex}")
+  resource_key_vault_name = lower("${var.resource_name}-${var.resource_deployment}-${random_id.id.hex}")
 }
